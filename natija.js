90fcc213f5479dbc0dfca5febc1f948337defbef
azizjon7683-source <azizjon7683@gmail.com>
2026-04-14 14:55:14 +0500

    natija login

---
+++ natija.js
@@ -0,0 +1,3 @@
+let supabaseKey = "sb_publishable_MFumWWURDMHmoCrOZcUrHg_yL8QFJu6"
+let supaBaseUrl="https://jtnugbdhpmtgplcymboe.supabase.co"
+const _supabase = supabase.createClient(supaBaseUrl, supabaseKey)
---
+++ scripttestlar.js
@@ -0,0 +1,22 @@
+
+const xabarCon = document.querySelector(".xabar-con");
+function xabarnoma(xabar, turi) {
+  let xabarMatn = document.createElement("div");
+  xabarMatn.classList.add("xabar", turi);
+  console.log(xabarMatn);
+
+  xabarMatn.innerText = xabar;
+
+  setTimeout(() => {
+    xabarMatn.remove();
+  }, 4000);
+
+  xabarCon.appendChild(xabarMatn);
+}
+function Boshlashi() {
+  xabarnoma('Testlar tez orada boshlanmoqda','info')
+  setTimeout(() => {
+    window.location.href='testlar.html'
+  }, 2500);
+}
+
